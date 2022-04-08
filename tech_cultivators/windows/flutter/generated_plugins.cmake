4ade91040834e5fe0639d5819a1d9e91db533c77
Soumitra <soumitrakand3@gmail.com>
2022-04-08 19:08:34 +0530

    News Page Updated

--- tech_cultivators/windows/flutter/generated_plugins.cmake
+++ tech_cultivators/windows/flutter/generated_plugins.cmake
@@ -5,12 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   syncfusion_pdfviewer_windows
   url_launcher_windows
-<<<<<<< HEAD
-)
-
-list(APPEND FLUTTER_FFI_PLUGIN_LIST
-=======
->>>>>>> 65d0b609079f2a2676af10b1fb52a994a0c2d7a9
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
